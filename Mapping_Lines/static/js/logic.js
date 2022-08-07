89e43fd31d2adc982e03b55a5b3f10ae30c17624
Jesus Capo <jesuscapo@Jesuss-iMac.local>
2022-08-07 07:16:57 -0500

    push

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,40 +2,20 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
-// // An array containing each city's location, state, and population.
-// let cities = [{
-//     location: [40.7128, -74.0059],
-//     city: "New York City",
-//     state: "NY",
-//     population: 8398748
-//   },
-//   {
-//     location: [41.8781, -87.6298],
-//     city: "Chicago",
-//     state: "IL",
-//     population: 2705994
-//   },
-//   {
-//     location: [29.7604, -95.3698],
-//     city: "Houston",
-//     state: "TX",
-//     population: 2325502
-//   },
-//   {
-//     location: [34.0522, -118.2437],
-//     city: "Los Angeles",
-//     state: "CA",
-//     population: 3990456
-//   },
-//   {
-//     location: [33.4484, -112.0740],
-//     city: "Phoenix",
-//     state: "AZ",
-//     population: 1660272
-//   }
-//   ];
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow"
+  }).addTo(map);
 
 // Get data from cities.js
 let cityData = cities;
@@ -51,7 +31,7 @@ cityData.forEach(function(city) {
    });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
